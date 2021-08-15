8976c4c31db2ae367e5f5f490e453d04846a8e9f
bharathram-sampath <bharathram.sampath@gmail.com>
2021-08-16 00:42:58 +0530

    logics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,95 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pricing-component';
+  @ViewChild('rangeValue', { static: true }) rangeValue: ElementRef;
+  @ViewChild('yearlyBill', { static: true }) yearlyBill: ElementRef;
+  reCalcPrice(){
+    this.calPageViews();
+    this.calCost();
+  }
+
+  calPageViews(){
+    switch(this.rangeValue.nativeElement.value){
+      case('1'):
+      {
+        return "10K"
+      }
+      case('2'):
+      {
+        return "50K"
+      }
+      case('3'):
+      {
+        return "100K"
+      }
+      case('4'):
+      {
+        return "500K"
+      }
+      case('5'):
+      {
+        return "1M"
+      }
+    }
+  }
+  calCost()
+  {
+    switch(this.rangeValue.nativeElement.value){
+      case('1'):
+      {
+        if(this.yearlyBill.nativeElement.checked)
+        {
+          return "$6.00";
+        }
+        else 
+        {
+          return "$8.00";
+        }
+      }
+      case('2'):
+      {
+        if(this.yearlyBill.nativeElement.checked)
+        {
+          return "$9.00";
+        }
+        else 
+        {
+          return "$12.00";
+        }
+      }
+      case('3'):
+      {
+        if(this.yearlyBill.nativeElement.checked)
+        {
+          return "$12.00";
+        }
+        else 
+        {
+          return "$16.00";
+        }
+      }
+      case('4'):
+      {
+        if(this.yearlyBill.nativeElement.checked)
+        {
+          return "$18.00";
+        }
+        else 
+        {
+          return "$24.00";
+        }
+      }
+      case('5'):
+      {
+        if(this.yearlyBill.nativeElement.checked)
+        {
+          return "$27.00";
+        }
+        else 
+        {
+          return "$36.00";
+        }
+      }
+    }  
+  }
 }
